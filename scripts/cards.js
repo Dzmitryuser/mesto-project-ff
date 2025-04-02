ec73eeebd96399e9a377a649bc24851dc8285523
my_laptop <dmitrij.kulik.d.v@gmail.com>
2025-04-02 17:47:31 +0300

    inicial_commit

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Арктика",
+      link: "https://images.unsplash.com/photo-1589656966895-2f33e7653819?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Мексика",
+      link: "https://images.unsplash.com/photo-1521216774850-01bc1c5fe0da?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Европа",
+      link: "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Азия",
+      link: "https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?q=80&w=2042&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Африка",
+      link: "https://images.unsplash.com/photo-1507461476191-0ed4f9f18533?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Антарктика",
+      link: "https://images.unsplash.com/photo-1551415923-a2297c7fda79?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+/* Определяем переменные */
+const cardTemplate = document.querySelector("#card-template").content;
+const placesItem = cardTemplate.querySelector(".places__item");
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+/* Функция добавления карточек */
+function addCard(photoLink, cardName) {
 
-// @todo: Функция создания карточки
+  /* Клонируем карточку */
+  const placeClonedCard = cardTemplate.cloneNode(true);
 
-// @todo: Функция удаления карточки
+  /* Наполняем элементы клонированной карточки*/
+  placeClonedCard.querySelector(".card__image").src = photoLink;
+  placeClonedCard.querySelector(".card__image").alt = cardName;
+  placeClonedCard.querySelector(".card__title").textContent = cardName;
 
-// @todo: Вывести карточки на страницу
+  /* Вызов функции удаления карточек со слушателем кнопки удаления */
+  const deleteButton = placeClonedCard.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+
+  /* Элемент функции добавления карточки, производящий непосредственное добавление*/
+  placesList.append(placeClonedCard);
+}
+
+/* первичный (при загрузке страницы) рендер карточек */
+for (i = 0; i <= initialCards.length - 1; i++) {
+  addCard(initialCards[i].link, initialCards[i].name);
+}
+
+/* Функция удаления карточки  от элемента на котором произошло событие*/
+function deleteCard(event) {
+  const cardToDelete = event.target.closest(".places__item");
+  cardToDelete.remove();
+}
